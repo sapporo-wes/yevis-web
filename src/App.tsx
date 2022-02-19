28dffc57b9e865deb91eeea1c6824f34eca9e2af
suecharo <suehiro619@gmail.com>
2022-02-19 18:29:25 +0900

    Add head hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { AccessAlarm } from '@mui/icons-material'
-import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import React from 'react'
+
+import MainHero from '@/components/MainHero'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#aca5c4',
+      main: '#6D6783',
+      dark: '#2a263e',
+    },
+    secondary: {
+      light: '#f1d69f',
+      main: '#dba00c',
+      dark: '#cf7b00',
+    },
+    background: {
+      default: '#e4e4e4',
+    },
+    error: {
+      main: '#bd3200',
+    },
+  },
+})
 
 const App: React.VFC = () => {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-      <AccessAlarm />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <MainHero />
+    </ThemeProvider>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
